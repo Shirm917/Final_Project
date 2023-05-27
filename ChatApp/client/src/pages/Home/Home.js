70893dd1f310bde77e660e838551d5c286bf1d1e
Shir Markowitz <shirm91799@gmail.com>
2023-05-27 00:19:13 -0400

    fixing look of homepage

--- ChatApp/client/src/pages/Home/Home.js
+++ ChatApp/client/src/pages/Home/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
         <section className="homeContainer">
             <ChatIcon className="chatIcon" sx={{width: 100, height: 100}} />
             <h1 className="homeTitle">Chat App</h1>
-            {/* Expanation: "A Chat App With Private Messaging and Temporary Group Messaging. In Chat Click Private Chat Tab To Chat Privately With Friends and Click Group Chat Tab To Join A Room and Chat With People In The Same Room. Rooms Are Temporary So Once You Join Another Or Logout, The Messages Will Disappear." */}
+            <p className="aboutText">A Chat App With Private Messaging and Temporary Group Messaging.</p>
         </section>
     );
 };
