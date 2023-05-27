d6814a38bb3c5ed41e770cd7b92c79e88e0e1d01
Shir Markowitz <shirm91799@gmail.com>
2023-05-26 23:14:11 -0400

    deleted typing animation module and from homepage

--- ChatApp/client/src/pages/Home/ExplanationArr.js
+++
@@ -1,3 +0,0 @@
-export const ExplanationArr = [
-  "A Chat App With Private Messaging and Temporary Group Messaging. In Chat Click Private Chat Tab To Chat Privately With Friends and Click Group Chat Tab To Join A Room and Chat With People In The Same Room. Rooms Are Temporary So Once You Join Another Or Logout, The Messages Will Disappear.",
-];
--- ChatApp/client/src/pages/Home/Home.js
+++ ChatApp/client/src/pages/Home/Home.js
@@ -1,16 +1,9 @@
-import {TypeAnimation} from "react-type-animation";
-import { ExplanationArr } from "./ExplanationArr";
-
 const Home = () => {
 
     return (
         <div className="homeContainer">
             <h1 className="homeTitle">Chat App</h1>
-            <TypeAnimation
-                sequence={ExplanationArr}
-                repeat={1}
-                wrapper="p"
-            />
+            {/* Expanation: "A Chat App With Private Messaging and Temporary Group Messaging. In Chat Click Private Chat Tab To Chat Privately With Friends and Click Group Chat Tab To Join A Room and Chat With People In The Same Room. Rooms Are Temporary So Once You Join Another Or Logout, The Messages Will Disappear." */}
         </div>
     );
 };
