9334d97de9344be1994d2017daaf0a0dc8ebe36a
Shir Markowitz <shirm91799@gmail.com>
2023-05-27 23:51:53 -0400

    added explation toggle to homepage

---
+++ ChatApp/client/src/pages/Home/Explanation.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Typography from "@mui/material/Typography";
+
+const Explanation = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <section className="explanationContainer">
+      <Typography
+        className="explanationTitle"
+        sx={{ fontSize: "20px" }}
+        onClick={() => setVisible(!visible)}
+      >
+        Learn More
+      </Typography>
+      <Typography
+        className="explanationPara"
+        sx={{ display: visible ? "block" : "none", fontSize: "15px" }}
+      >
+        In Chat Click Private Chat Tab To Chat Privately With Friends and Click
+        Group Chat Tab And Enter Any Text To Join A Room and Chat With People
+        Who Joined The Same Room. Rooms Are Temporary So Once You Join Another
+        Or Logout, The Messages Will Disappear.
+      </Typography>
+    </section>
+  );
+};
+
+export default Explanation;
--- ChatApp/client/src/pages/Home/Home.js
+++ ChatApp/client/src/pages/Home/Home.js
@@ -1,16 +1,19 @@
-import ChatIcon from '@mui/icons-material/Chat';
-import Buttons from './Buttons';
+import ChatIcon from "@mui/icons-material/Chat";
+import Buttons from "./Buttons";
+import Explanation from "./Explanation";
 
 const Home = () => {
-
-    return (
-        <section className="homeContainer">
-            <ChatIcon className="chatIcon" sx={{width: 100, height: 100}} />
-            <h1 className="homeTitle">Chat App</h1>
-            <p className="aboutText">A Chat App With Private Messaging and Temporary Group Messaging.</p>
-            <Buttons/>
-        </section>
-    );
+  return (
+    <section className="homeContainer">
+      <ChatIcon className="chatIcon" sx={{ width: 100, height: 100 }} />
+      <h1 className="homeTitle">Chat App</h1>
+      <p className="aboutText">
+        A Chat App With Private Messaging and Temporary Group Messaging.
+      </p>
+      <Buttons />
+      <Explanation/>
+    </section>
+  );
 };
 
 export default Home;
