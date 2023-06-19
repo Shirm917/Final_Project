0907775995681c273bca10d61f5e8e6c08c59358
Shir Markowitz <shirm91799@gmail.com>
2023-06-18 23:52:22 -0400

    fixing reload and input

--- ChatApp/client/src/pages/PrivateChat/pageComponents/ChatInput.js
+++ ChatApp/client/src/pages/PrivateChat/pageComponents/ChatInput.js
@@ -1,35 +1,37 @@
-import {useState,useEffect,useContext} from "react";
-import {socket} from "../../../utils/socket";
+import { useState, useEffect, useContext } from "react";
+import { socket } from "../../../utils/socket";
 import { AppContext } from "../../../contexts/AppContext";
 import axios from "axios";
-import TextField from '@mui/material/TextField';
+import TextField from "@mui/material/TextField";
 
 const ChatInput = () => {
-  const {fromUserId,toUserId,showChat,fromUsername} = useContext(AppContext);
-  const [text,setText] = useState("");
-
+  const { fromUserId, toUserId, showChat, fromUsername } =
+    useContext(AppContext);
+  const [text, setText] = useState("");
 
   useEffect(() => {
     if (fromUserId) {
-        socket.emit("fromUserId", fromUserId);
-    };
-  },[]);
+      socket.emit("fromUserId", fromUserId);
+    }
+  }, []);
 
   const sendMessage = () => {
     if (toUserId && text && fromUsername) {
-      socket.emit("chat message", toUserId,text,fromUsername);
-    };
+      const processedText = text.replace(/ +/g, "\u00A0");
+      socket.emit("chat message", toUserId, processedText, fromUsername);
+    }
     setText("");
   };
 
-  const postMessage = async() => {
+  const postMessage = async () => {
     const dateNow = new Date();
-    if (text){
+    const processedText = text.replace(/ +/g, "\u00A0");
+    if (text) {
       await axios.post("/messages", {
         text,
         fromUserId,
         toUserId,
-        timestamp: dateNow.toUTCString()
+        timestamp: dateNow.toUTCString(),
       });
     }
     setText("");
@@ -46,27 +48,26 @@ const ChatInput = () => {
       event.preventDefault();
       handleSubmit(event);
     }
-  }
+  };
 
-  return (
-    !showChat ? ""
-    :
+  return !showChat ? (
+    ""
+  ) : (
     <form className="chatForm" onSubmit={handleSubmit}>
       <TextField
-      sx={{marginLeft: { xs: "0px", sm: "200px" }}}
-      className="textfield"
-      id="outlined-multiline-flexible"
-      helperText="Shift + Return/Enter for new line"
-      value={text}
-      autoComplete="off"
-      multiline
-      // style={{ whiteSpace: 'pre-wrap' }}
-      InputProps={{endAdornment: <button className="btn">Send</button>}}
-      onChange={(event) => setText(event.target.value)}
-      onKeyDown={handleKeyDown}
+        sx={{ marginLeft: { xs: "0px", sm: "200px" } }}
+        className="textfield"
+        id="outlined-multiline-flexible"
+        helperText="Shift + Return/Enter for new line"
+        value={text}
+        autoComplete="off"
+        multiline
+        InputProps={{ endAdornment: <button className="btn">Send</button> }}
+        onChange={(event) => setText(event.target.value)}
+        onKeyDown={handleKeyDown}
       />
     </form>
   );
-}
+};
 
 export default ChatInput;
