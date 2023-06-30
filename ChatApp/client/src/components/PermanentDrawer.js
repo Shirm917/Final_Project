f9b90189917ad119729d14bd023a7500a91043f2
Shir Markowitz <shirm91799@gmail.com>
2023-06-29 23:31:26 -0400

    changed margin of permanant drawer

--- ChatApp/client/src/components/PermanentDrawer.js
+++ ChatApp/client/src/components/PermanentDrawer.js
@@ -1,27 +1,27 @@
 import Drawer from "@mui/material/Drawer";
 
-const PermanentDrawer = ({drawer}) => {
-    return (
-        <Drawer
-        variant="permanent"
-        anchor="left"
-        sx={{
-          display: { xs: "none", sm: "block" },
+const PermanentDrawer = ({ drawer }) => {
+  return (
+    <Drawer
+      variant="permanent"
+      anchor="left"
+      sx={{
+        display: { xs: "none", sm: "block" },
+        width: 200,
+        flexShrink: 0,
+        zIndex: 0,
+        "& .MuiDrawer-paper": {
+          boxSizing: "border-box",
           width: 200,
-          flexShrink: 0,
-          zIndex: 0,
-          "& .MuiDrawer-paper": {
-            boxSizing: "border-box",
-            width: 200,
-            marginTop: 7.6,
-            marginRight: 20,
-          },
-        }}
-        open
-      >
-        {drawer}
-      </Drawer>
-     );
-}
+          marginTop: 6.6,
+          marginRight: 20,
+        },
+      }}
+      open
+    >
+      {drawer}
+    </Drawer>
+  );
+};
 
 export default PermanentDrawer;
